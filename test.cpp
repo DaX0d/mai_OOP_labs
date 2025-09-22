43da06aad7a1678aeb26aaaad3ff9255076033bc
DaX0d <daniil.khodyrev@gmail.com>
2025-09-22 20:47:24 +0300

    more tests added

--- test.cpp
+++ test.cpp
@@ -9,6 +9,14 @@ TEST(test_2, basic_test_set) {
     ASSERT_TRUE(find_num("dsjhfg123456hdf90") == 12345690);
 }
 
+TEST(test_3, basic_test_set) {
+    ASSERT_TRUE(find_num("a") == 0);
+}
+
+TEST(test_4, basic_test_set) {
+    ASSERT_TRUE(find_num("1234567890dsfhgwdhf162547") == 1234567890162547);
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
