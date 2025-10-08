a17ed1c599554d5db4f62016401f90e1062283da
DaX0d <daniil.khodyrev@gmail.com>
2025-10-08 19:21:47 +0300

    laba2: интерфейс готов

--- laba_2/main.cpp
+++ laba_2/main.cpp
@@ -10,8 +10,8 @@ static void print_menu() {
     cout << "4) Вывести число как десятичное\n";
     cout << "5) Вывести сумму с десятичным числом\n";
     cout << "6) Вывести сумму с тринадцатеричным числом\n";
-    cout << "7) Прибавить / вычесть десятичное число\n";
-    cout << "8) Прибавить / вычесть тринадцатеричное число\n";
+    cout << "7) Прибавить десятичное число\n";
+    cout << "8) Прибавить тринадцатеричное число\n";
     cout << "0) Выход\n";
     cout << "\nДействие: ";
 }
@@ -40,14 +40,46 @@ int main() {
                 number = new Thirteen(tmp_str);
             break;
             case '3':
-                std::cout << number->get_as_string();
+                std::cout << number->get_as_string() << "\n";
             break;
             case '4':
-                std::cout << number->get_as_int10();
+                std::cout << number->get_as_int10() << "\n";
+            break;
+            case '5':
+                std::cout << "\nВведите число: ";
+                std::cin >> tmp_str;
+                tmp_int = std::stoull(tmp_str);
+                {
+                    Thirteen tmp = number->plus(Thirteen(tmp_int));
+                    std::cout << tmp.get_as_string() << " (";
+                    std::cout << tmp.get_as_int10() << ")\n";
+                }
+            break;
+            case '6':
+                std::cout << "\nВведите число: ";
+                std::cin >> tmp_str;
+                {
+                    Thirteen tmp = number->plus(Thirteen(tmp_str));
+                    std::cout << tmp.get_as_string() << " (";
+                    std::cout << tmp.get_as_int10() << ")\n";
+                }
+            break;
+            case '7':
+                std::cout << "\nВведите число: ";
+                std::cin >> tmp_str;
+                tmp_int = std::stoull(tmp_str);
+                number->pluseq(Thirteen(tmp_int));
+            break;
+            case '8':
+                std::cout << "\nВведите число: ";
+                std::cin >> tmp_str;
+                number->pluseq(Thirteen(tmp_str));
             break;
         }
+        print_menu();
+        std::cin >> choice;
     }
-    
+
     delete number;
     return 0;
 }
