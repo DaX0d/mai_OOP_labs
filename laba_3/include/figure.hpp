7707485c93649e4888041d7566ab8e520edd861d
DaX0d <daniil.khodyrev@gmail.com>
2025-10-16 23:19:30 +0300

    laba3: переделан Figure

--- laba_3/include/figure.hpp
+++ laba_3/include/figure.hpp
@@ -1,11 +1,25 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+
+#include "point.hpp"
 
 class Figure {
 public:
     Figure() = default;
     virtual ~Figure() {}
 
-    virtual std::ostream& operator<<(std::ostream& out);
+    virtual Point center() const;
+
+    virtual operator double() const;
+
+    friend std::ostream& operator<<(std::ostream& out, Figure& fig) {return fig.__write(out);}
+    friend std::istream& operator>>(std::istream& inp, Figure& fig) {return fig.__read(inp);}
+
+protected:
+    std::vector<Point> __points;
+
+    virtual std::ostream& __write(std::ostream& out) const;
+    virtual std::istream& __read(std::istream& inp);
 };
---
+++ laba_3/include/hexagon.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "figure.hpp"
+
+class Hexagon: public Figure {
+public:
+    Hexagon();
+};
